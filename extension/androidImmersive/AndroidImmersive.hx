31138e4e1c20474d0f06828014f68be3ec4e456a
Will Blanton <will.blanton.804@gmail.com>
2018-06-20 16:44:02 -0400

    Updating JNI references

--- extension/androidImmersive/AndroidImmersive.hx
+++ extension/androidImmersive/AndroidImmersive.hx
@@ -1,11 +1,19 @@
 package extension.androidImmersive;
 
+#if android
+	#if (openfl < "4.0.0")
+	import openfl.utils.JNI;
+	#else
+	import lime.system.JNI;
+	#end
+#end
+
 class AndroidImmersive {
 	
 	#if (android)
-		public static var setStatusBarColor:Int->Void = openfl.utils.JNI.createStaticMethod("org/haxe/extension/AndroidImmersive", "setStatusBarColor", "(I)V");
-		public static var setLowProfile:Void->Void = openfl.utils.JNI.createStaticMethod("org/haxe/extension/AndroidImmersive", "setLowProfile", "()V");
-		public static var setImmersive:Void->Void = openfl.utils.JNI.createStaticMethod("org/haxe/extension/AndroidImmersive", "setImmersive", "()V");
+		public static var setStatusBarColor:Int->Void = JNI.createStaticMethod("org/haxe/extension/AndroidImmersive", "setStatusBarColor", "(I)V");
+		public static var setLowProfile:Void->Void = JNI.createStaticMethod("org/haxe/extension/AndroidImmersive", "setLowProfile", "()V");
+		public static var setImmersive:Void->Void = JNI.createStaticMethod("org/haxe/extension/AndroidImmersive", "setImmersive", "()V");
 	#else
 		public static var setStatusBarColor:Int->Void = function(i:Int){};
 		public static var setLowProfile:Void->Void = function(){};
